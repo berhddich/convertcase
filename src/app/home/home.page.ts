b81d3d3c12e0526a3fb0d6e01cd858aff4579d13
rahim br <berhi93ddich@outlook.com>
2022-07-15 16:17:44 +0100

    cmt

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import voca from 'voca';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  entrytext: string;
+  finaltext: string;
+  voca;
   constructor() {}
-
+  lowerCase(): void {
+    this.finaltext = voca.lowerCase(this.entrytext);
+  }
+  upperCase(): void {
+    this.finaltext = voca.upperCase(this.entrytext);
+  }
+  camelCase(): void {
+    this.finaltext = voca.camelCase(this.entrytext);
+  }
+  slugify(): void {
+    this.finaltext = voca.slugify(this.entrytext);
+  }
+  capitalize(): void {
+    this.finaltext = voca.capitalize(this.entrytext);
+  }
+  decapitalize(): void {
+    this.finaltext = voca.decapitalize(this.entrytext);
+  }
+  kebabCase(): void {
+    this.finaltext = voca.kebabCase(this.entrytext);
+  }
+  snakeCase(): void {
+    this.finaltext = voca.snakeCase(this.entrytext);
+  }
+  titleCase(): void {
+    this.finaltext = voca.titleCase(this.entrytext);
+  }
 }
